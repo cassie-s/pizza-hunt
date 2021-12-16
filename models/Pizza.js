eb4843654cfd5de5b2d5305e05082b7d6d4a120a
cassie-s <56046933+cassie-s@users.noreply.github.com>
2021-12-16 17:07:39 -0600

    implement replies

--- models/Pizza.js
+++ models/Pizza.js
@@ -33,9 +33,9 @@ const PizzaSchema = new Schema({
     id: false
   });
 
-PizzaSchema.virtual('commentCount').get(function() {
-    return this.comments.length;
-});
+  PizzaSchema.virtual('commentCount').get(function() {
+    return this.comments.reduce((total, comment) => total + comment.replies.length + 1, 0);
+  });
 
   // create the Pizza model using the PizzaSchema
 const Pizza = model('Pizza', PizzaSchema);
